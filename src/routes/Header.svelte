1e34c61f6e16de98e78253c1909f0e317a86d9fa
Gonzalo Ruiz <rgon@rgon.es>
2023-01-17 05:34:56 +0100

    Remove dead header link to unfinished part of app

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -29,9 +29,9 @@
 			<li aria-current={$page.url.pathname.startsWith('/list') ? 'page' : undefined}>
 				<a href="/list">History</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/bankLink') ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname.startsWith('/bankLink') ? 'page' : undefined}>
 				<a href="/bankLink">Bank Link</a>
-			</li>
+			</li> -->
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
